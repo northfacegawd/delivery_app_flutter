1c2d85b70627b90c4bf4386ff3db70c8ccaf320b
northfacegawd <chuck5732590@gmail.com>
2023-09-01 23:14:39 +0900

    feat: on error 로그 추가

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -36,7 +36,8 @@ class CustomInterceptor extends Interceptor {
   // 2) 응답을 받을때
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    // TODO: implement onResponse
+    print(
+        '[RESPONSE] [${response.requestOptions.method}] ${response.requestOptions.uri}');
     return super.onResponse(response, handler);
   }
 
