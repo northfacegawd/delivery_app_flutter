427496c7edb8d0e029957b5ae56be87579018f29
northfacegawd <chuck5732590@gmail.com>
2023-09-01 23:11:33 +0900

    feat: 401에러 발생시 accessToken 리프레시

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -37,13 +37,52 @@ class CustomInterceptor extends Interceptor {
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
     // TODO: implement onResponse
-    super.onResponse(response, handler);
+    return super.onResponse(response, handler);
   }
 
   // 3) 에러가 났을때
   @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    // TODO: implement onError
-    super.onError(err, handler);
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    print('[ERROR] [${err.requestOptions.method}] ${err.requestOptions.uri}');
+
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+
+    // refreshToken이 없으면 에러를 던짐
+    if (refreshToken == null) {
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+      try {
+        final response = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(
+            headers: {
+              'Authorization': 'Bearer $refreshToken',
+            },
+          ),
+        );
+        final options = err.requestOptions;
+        final accessToken = response.data['accessToken'];
+        options.headers.addAll(
+          {'Authorization': 'Bearer $accessToken'},
+        );
+        await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+        // 요청 재전송
+        final retryResponse = await dio.fetch(options);
+        return handler.resolve(retryResponse);
+      } on DioException catch (e) {
+        return handler.reject(e);
+      }
+    }
+
+    // return handler.resolve()
+
+    // 401 에러인 경우 accessToken 재발급 시도
+    return super.onError(err, handler);
   }
 }
