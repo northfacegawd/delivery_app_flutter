454164e9d53a57d3b6a0082ff19f29662e2a5d4b
northfacegawd <chuck5732590@gmail.com>
2023-09-11 17:43:48 +0900

    feat: product tab 페이지네이션 적용

--- lib/product/views/product_tab.dart
+++ lib/product/views/product_tab.dart
@@ -1,10 +1,19 @@
+import 'package:delivery_app/product/provider/product_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProductTab extends StatelessWidget {
+class ProductTab extends ConsumerStatefulWidget {
   const ProductTab({super.key});
 
   @override
+  ConsumerState<ProductTab> createState() => _ProductTabState();
+}
+
+class _ProductTabState extends ConsumerState<ProductTab> {
+  @override
   Widget build(BuildContext context) {
+    final state = ref.watch(productProvider);
+
     return const Center(
       child: Text("음식"),
     );
