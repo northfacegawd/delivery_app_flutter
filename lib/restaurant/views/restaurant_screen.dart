3a77738f89aa287a82c6ed776be2406676f50601
northfacegawd <chuck5732590@gmail.com>
2023-09-11 18:06:56 +0900

    feat: 일반화된 페이지네이션 리스트 뷰 레스토랑 스크린에 적용

--- lib/restaurant/views/restaurant_screen.dart
+++ lib/restaurant/views/restaurant_screen.dart
@@ -1,34 +1,12 @@
 import 'package:delivery_app/common/components/pagination_list_view.dart';
-import 'package:delivery_app/common/utils/pagination_util.dart';
 import 'package:delivery_app/restaurant/components/restaurant_card.dart';
 import 'package:delivery_app/restaurant/models/restaurant_model.dart';
 import 'package:delivery_app/restaurant/provider/restaurant_provider.dart';
 import 'package:delivery_app/restaurant/views/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerStatefulWidget {
+class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
-
-  @override
-  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
-}
-
-class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
-  final ScrollController _controller = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(scrollListener);
-  }
-
-  void scrollListener() {
-    PaginationUtils.paginate(
-      controller: _controller,
-      provider: ref.read(restaurantProvider.notifier),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
