42a48ad2b9933b9459275bda3c9628e099c1fb0f
northfacegawd <chuck5732590@gmail.com>
2023-09-11 13:21:09 +0900

    feat: rating 모델링

--- lib/common/utils/data_util.dart
+++ lib/common/utils/data_util.dart
@@ -1,5 +1,7 @@
 import 'package:delivery_app/auth/constants/data.dart';
 
 class DataUtils {
-  static pathToUrl(String value) => 'http://$ip$value';
+  static String pathToUrl(String value) => 'http://$ip$value';
+  static List<String> listPathsToUrls(List<String> paths) =>
+      paths.map((e) => pathToUrl(e)).toList();
 }
