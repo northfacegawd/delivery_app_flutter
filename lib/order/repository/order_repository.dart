80180e1dc5a33e042237e6ab2572a5cf73f0ac2f
northfacegawd <chuck5732590@gmail.com>
2023-09-14 00:08:49 +0900

    feat: order repository 생성

---
+++ lib/order/repository/order_repository.dart
@@ -0,0 +1,24 @@
+import 'package:delivery_app/common/dio/dio.dart';
+import 'package:delivery_app/order/models/order_model.dart';
+import 'package:delivery_app/order/models/post_order_body.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'order_repository.g.dart';
+
+final orderRepositoryProvider = Provider((ref) {
+  final dio = ref.watch(dioProvider);
+  return OrderRepository(dio);
+});
+
+@RestApi()
+abstract class OrderRepository {
+  factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @POST("/")
+  @Headers({'accessToken': 'true'})
+  Future<OrderModel> postOrder({
+    @Body() required PostOrderBody body,
+  });
+}
